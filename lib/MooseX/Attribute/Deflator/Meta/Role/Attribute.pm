1d8fc2c4a0e64de02c54a25b086e572de6349643
Moritz Onken <onken@netcubed.de>
2010-03-30 19:00:20 +0800

    consolidated inflate/deflate methods

--- lib/MooseX/Attribute/Deflator/Meta/Role/Attribute.pm
+++ lib/MooseX/Attribute/Deflator/Meta/Role/Attribute.pm
@@ -1,53 +1,39 @@
 package MooseX::Attribute::Deflator::Meta::Role::Attribute;
 
+# ABSTRACT: Attribute meta role to support deflation
 use Moose::Role;
 use MooseX::Attribute::Deflator;
 my $REGISTRY = MooseX::Attribute::Deflator->get_registry;
 no MooseX::Attribute::Deflator;
 
-sub has_deflator {
-	my ($self) = @_;
-	return$REGISTRY->get_deflator($self->type_constraint->name);
-}
+foreach my $m (qw( deflator inflator)) {
+    my $get = 'get_' . $m;
+    ( my $action = $m ) =~ s/or/e/;
+    __PACKAGE__->meta->add_method(
+        $action => sub {
+            my ( $self, $obj, $value, $constraint, @rest ) = @_;
+            $value ||= $self->get_value($obj);
+            $constraint ||= $self->type_constraint;
+            ( my $name = $constraint->name ) =~ s/\[.*\]/\[\]/;
+            if ( my $via = $REGISTRY->$get($name) ) {
+                return $via->(
+                    $obj, $constraint, sub { $self->$action( $obj, @_ ) }, @rest
+                ) for ($value);
+            }
+            else {
+                return $self->$action( $obj, $value, $constraint->parent,
+                    @rest )
+                  if ( $constraint->has_parent );
+                Moose->throw_error( "Cannot $action " . $self->name );
+            }
+        }
+    );
 
-use Data::Dumper;
-$Data::Dumper::Indent = 1;
-$Data::Dumper::Maxdepth = 2;
-
-sub deflate {
-	my ($self, $obj, $value, $constraint, @rest) = @_;
-    $value ||= $self->get_value($obj);
-    $constraint ||= $self->type_constraint;
-    (my $name = $constraint->name) =~ s/\[.*\]/\[\]/;
-    my $via = $REGISTRY->get_deflator($name);
-    unless($via) {
-
-        return $self->deflate($obj, $value, $constraint->parent, @rest) if($constraint->has_parent);
-        
-    
-    }
-	Moose->throw_error('Cannot deflate ' . $self->name) unless($via);
-	return $via->($obj, $constraint, sub { $self->deflate($obj, @_) }, @rest ) for($value);
-}
-
-
-sub has_inflator {
-	my ($self) = @_;
-	return $REGISTRY->get_inflator($self->type_constraint->name);
-}
-
-sub inflate {
-	my ($self, $obj, $value, $constraint, @rest) = @_;
-    $value ||= $self->get_value($obj);
-    $constraint ||= $self->type_constraint;
-	(my $name = $constraint->name) =~ s/\[.*\]/\[\]/;
-    my $via = $REGISTRY->get_inflator($name);
-
-    unless($via) {
-        return $self->inflate($obj, $value, $constraint->parent, @rest) if($constraint->has_parent);
-    }
-    Moose->throw_error('Cannot inflate ' . $self->name) unless($via);
-    return $via->($obj, $constraint, sub { $self->inflate($obj, @_) }, @rest) for($value);
+    __PACKAGE__->meta->add_method(
+        'has_' . $m => sub {
+            return $REGISTRY->$get( shift->type_constraint->name );
+        }
+    );
 }
 
 1;
